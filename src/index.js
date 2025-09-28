66966e294d1d481d83abf59be5a86dc81dc63cb2
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-09-28 12:42:22 +0530

    main

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import cors from "cors"
 import dbConnect from './utils/mongodb.js';
 import { PORT } from './config/config.js';
 import formRouter from './routes/form.route.js';
+import complainFormRouter from './routes/complainform.route.js';
+import admissionRouter from './routes/admissionform.route.js';
 
 const app = express()
 app.use(cors());
 app.use(express.json());
-app.use('/post',formRouter)
-app.use('/get',formRouter)
-app.use('/delete',formRouter)
+app.use('/form',formRouter)
+app.use('/admission-form',admissionRouter)
+app.use('/complaint',complainFormRouter)
 
 app.get("/", (req, res) => {
   res.send("API is running...");
